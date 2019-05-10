b72769c92c99b431cdc4f778742c5ef0abb5e5b9
Antoine Gersant <antoine.gersant@lesforges.org>
2019-05-09 21:02:10 -0700

    Re-ordered errors

--- src/state/error.rs
+++ src/state/error.rs
@@ -4,16 +4,24 @@ pub enum StateError {
     NoDocumentOpen,
     #[fail(display = "Requested document was not found")]
     DocumentNotFound,
-    #[fail(display = "Sheet has no export settings")]
-    NoExistingExportSettings,
     #[fail(display = "Cannot perform undo operation")]
     UndoOperationNowAllowed,
+    #[fail(display = "Sheet has no export settings")]
+    NoExistingExportSettings,
     #[fail(display = "Requested frame is not in document")]
     FrameNotInDocument,
     #[fail(display = "Requested animation is not in document")]
     AnimationNotInDocument,
+    #[fail(display = "Frame does not have a hitbox with the requested name")]
+    InvalidHitboxName,
+    #[fail(display = "Animation does not have a frame at the requested index")]
+    InvalidAnimationFrameIndex,
+    #[fail(display = "No animation frame found for requested time")]
+    NoAnimationFrameForThisTime,
     #[fail(display = "Expected a hitbox to be selected")]
     NoHitboxSelected,
+    #[fail(display = "Expected an animation frame to be selected")]
+    NoAnimationFrameSelected,
     #[fail(display = "A hitbox with this name already exists")]
     HitboxAlreadyExists,
     #[fail(display = "An animation with this name already exists")]
@@ -22,28 +30,20 @@ pub enum StateError {
     NotEditingAnyFrame,
     #[fail(display = "Not currently editing any animation")]
     NotEditingAnyAnimation,
-    #[fail(display = "Frame does not have a hitbox with the requested name")]
-    InvalidHitboxName,
-    #[fail(display = "Expected an animation frame to be selected")]
-    NoAnimationFrameSelected,
-    #[fail(display = "Animation does not have a frame at the requested index")]
-    InvalidAnimationFrameIndex,
-    #[fail(display = "No animation frame found for requested time")]
-    NoAnimationFrameForThisTime,
     #[fail(display = "Not currently adjusting export settings")]
     NotExporting,
     #[fail(display = "Not currently renaming an item")]
     NotRenaming,
     #[fail(display = "Not currently adjusting animation frame position")]
     NotAdjustingAnimationFramePosition,
-    #[fail(display = "Not currently adjusting animation frame duration")]
-    NotAdjustingAnimationFrameDuration,
     #[fail(display = "Not currently adjusting hitbox size")]
     NotAdjustingHitboxSize,
-    #[fail(display = "Missing data while adjusting hitbox size")]
-    MissingHitboxSizeData,
     #[fail(display = "Not currently adjusting hitbox position")]
     NotAdjustingHitboxPosition,
+    #[fail(display = "Not currently adjusting animation frame duration")]
+    NotAdjustingAnimationFrameDuration,
+    #[fail(display = "Missing data while adjusting hitbox size")]
+    MissingHitboxSizeData,
     #[fail(display = "Missing data while adjusting hitbox position")]
     MissingHitboxPositionData,
     #[fail(display = "Missing data while adjusting animation frame position")]
